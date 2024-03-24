026d465d553cbd9be931b358c23c3b3fe392a4d1
Ghalaalaskar <144720600+Ghalaalaskar@users.noreply.github.com>
2024-03-24 04:49:31 +0300

    Add files via upload

---
+++ CUpdate.php
@@ -0,0 +1,18 @@
+<?php
+$connection= mysqli_connect("localhost","root","root","artiscape");
+        if(mysqli_connect_error()!= null){
+            echo'An error occur in database connection';
+            die(mysqli_conncet_error());}  
+        else{
+    if (isset($_GET['updatedid'])) {
+    $updatedid = $_GET['updatedid'];
+
+    $sql = "Update FROM DesignConsultationRequest SET statusID='2' WHERE id = $updatedid";
+   if (mysqli_query($connection, $sql)) {
+   echo '<script>alert ("The consulation is declined.")</script>';
+   header("Location: DesignerHomepage.php");
+   } else {
+   echo '<script>alert("Error while declining the request")</script>';
+   }
+}
+        }
---
+++ PDelete.php
@@ -0,0 +1,20 @@
+<?php
+        $connection= mysqli_connect("localhost","root","root","artiscape");
+        if(mysqli_connect_error()!= null){
+            echo'An error occur in database connection';
+            die(mysqli_conncet_error());}
+            
+        else{
+        if (isset($_GET['project_id'])) {
+        $projectID = $_GET['project_id'];
+        $sql = "DELETE FROM DesignPortoflioProject WHERE id = $projectID";
+        if (mysqli_query($connection, $sql)) {
+        echo '<script>alert ("Project deleted succesfully.")</script>';
+        header("Location: DesignerHomepage.php");
+        } else {
+        echo '<script>alert("Error while deleting the project")</script>';
+   }
+    }
+}
+        
+?>
